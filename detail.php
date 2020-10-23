d048677d592b0f48256434b991f2e773dd64c715
flaforti <38009890+flaforti@users.noreply.github.com>
2020-10-23 19:41:48 +0200

    1

--- detail.php
+++ detail.php
@@ -7,7 +7,8 @@ require __DIR__ .  '/vendor/autoload.php';
 // Agrega credenciales
 MercadoPago\SDK::setAccessToken('APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398');
 
-MercadoPago\SDK::setIntegratorId("dev_24c65fb163bf11ea96500242ac130004");
+// Error con setIntegratorId
+//MercadoPago\SDK::setIntegratorId("dev_24c65fb163bf11ea96500242ac130004");
 
 // Crea un objeto de preferencia
 $preference = new MercadoPago\Preference();
